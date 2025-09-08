314c65a0f086abea97e705e7b52f5f0179be2fe8
Jubael Mamon <jubaelmamon@jmac.local>
2025-09-07 22:39:38 -0400

    local changes

---
+++ app/(auth)/auth/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import {useState} from 'react';
+import {Button} from '@/components/ui/button';
+import { ChevronLeft } from 'lucide-react';
+export default function Auth() {
+  return (
+    <div>
+    AuthPage
+    </div>
+  )
+}
---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage () {
+  return (
+    <div className="text-4xl font-extrabold text-red-500 tracking-wide drop-shadow-lg">
+      HomePage
+    </div>
+  )
+}
